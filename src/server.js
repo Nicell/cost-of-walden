44ca28d8380becfe7e8a0857229652a087b52e98
Nick <nick.win999@gmail.com>
2020-03-23 21:13:31 -0500

    Fix baseURL

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'/cost-of-walden',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
